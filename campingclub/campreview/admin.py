e1a1587b844c96f2c222a4ec82c0623ce7a85e7f
janainfanger <43797937+janainfanger@users.noreply.github.com>
2019-03-13 12:12:35 -0700

    Adds Models and Super User
    superuser jana pw chaud pantalon reg
    works!

--- campingclub/campreview/admin.py
+++ campingclub/campreview/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Campground, Campsite, CampsiteFeatures, Features
 
 # Register your models here.
+
+admin.site.register(Campground)
+admin.site.register(Features)
+admin.site.register(CampsiteFeatures)
+admin.site.register(Campsite)
--- campingclub/campreview/models.py
+++ campingclub/campreview/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Features(models.Model):
+    featurename=models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.featurename
+
+    class Meta:
+        db_table='Features'
+
+
+class Campground(models.Model):
+    name=models.CharField(max_length=255)
+    address=models.CharField(max_length=255)
+    features=models.ManyToManyField(Features)
+    description=models.TextField(max_length=255)
+    
+    def __str__(self):
+        return self.name  
+
+    class Meta:
+        db_table='Campground'
+
+class CampsiteFeatures(models.Model):
+    viewtype=models.CharField(max_length=100)
+    trees=models.CharField(max_length=100)
+    partysize=models.IntegerField()
+    privacy=models.CharField(max_length=50)
+   
+    def __str__(self):
+        return self.viewtype
+
+    class Meta:
+        db_table='CampsiteFeatures'
+
+
+class Campsite(models.Model):
+    CampgroundID=models.ForeignKey(Campground, on_delete=models.DO_NOTHING)
+    campsiteID=models.CharField(max_length=3)
+    campsitefeatures=models.ManyToManyField(CampsiteFeatures)
+
+    def __str__(self):
+        return self.campsiteID
+
+    class Meta:
+        db_table='Campsite'
+
+
