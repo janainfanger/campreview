6b7d82ea68eed66b2b3796739d03117bd04f81e9
janainfanger <43797937+janainfanger@users.noreply.github.com>
2019-03-13 12:29:10 -0700

    Adds Campground view
    First view is working

--- campingclub/campingclub/urls.py
+++ campingclub/campingclub/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('campreview/', include('campreview.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    
 ]
--- campingclub/campreview/views.py
+++ campingclub/campreview/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, get_object_or_404
-
+from.models import Campground, Campsite, CampsiteFeatures, Features
 
 # Create your views here.
 
 def index(request):
     return render(request, 'campreview/index.html')
+
+def campground(request):
+    campground=Campground.objects.all()
+    return render(request, 'campreview/campground.html', {'campground': campground})
+
 
     
